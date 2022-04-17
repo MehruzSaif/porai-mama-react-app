11b52a0addb57ed9929c0c4060803630c4ccf5e1
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-18 03:34:24 +0600

    added Login component and form

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import NF404 from '../../../images/NF404.jpg';
 
 const NotFound = () => {
     return (
-        <div className='container'>
+        <div className='container mx-auto'>
             <h1 className='text-center mt-5'>Page Not Found</h1>
             <img className='w-100' src={NF404} alt="" />
         </div>
