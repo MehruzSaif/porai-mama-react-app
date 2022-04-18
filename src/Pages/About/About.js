2ba4d0b9b1edab363e3bdcde437f48393e66ad0a
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-18 23:23:34 +0600

    about page changed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -10,8 +10,8 @@ const About = () => {
                 <Card.Img className='about-img' variant="top" src={ownpic} />
                 <Card.Body>
                     <Card.Title className='mb-4 blog-title'>HELLO THERE. I'M <span className='about-name'>MEHRUZ SAIF</span></Card.Title>
-                    <Card.Text>
-                        A quick and keen but <span className='fakibaj'>ফাঁকিবাজ</span> learner who wants to improve skills as a Full-Stack Web Developer. Highly organized, responsible, outgoing, technology savvy with ideas, adaptable in any scenario, and demonstrated capacity to improve oneself and others. <br />
+                    <Card.Text className='about-text'>
+                        A quick and keen but <span className='fakibaj'>ফাঁকিবাজ</span> learner who wants to improve skills as a Full-Stack Web Developer. Highly organized, responsible, outgoing, technology savvy with ideas, adaptable in any scenario, and demonstrated capacity to improve oneself and others. <br /><br />
                         Since enrolling in the Programming Hero Web Development course, I've been creating code every day. Alhumdulillah! I can tell that I'm getting better every day. As if I were performing an assignment alone, I challenge myself with a new skill. Will contribute to open-source projects and will always teach what I know to others. It greatly aids my talent development. I set aside at least 6-8 hours every day for regular downtime, as per my timetable.
                     </Card.Text>
                 </Card.Body>
