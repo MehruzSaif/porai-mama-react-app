6bd88aeaff422f58024ffda5f0905906a5d30c12
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-18 21:30:05 +0600

    added about route and added things on it

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import ownpic from '../../images/ownpic.png';
+import './About.css';
 
 const About = () => {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <div className='container'>
+            <Card className='my-5 about-card'>
+                <Card.Img className='about-img' variant="top" src={ownpic} />
+                <Card.Body>
+                    <Card.Title className='mb-4 blog-title'>HELLO THERE. I'M <span className='about-name'>MEHRUZ SAIF</span></Card.Title>
+                    <Card.Text>
+                        A quick and keen but <span className='fakibaj'>ফাঁকিবাজ</span> learner who wants to improve skills as a Full-Stack Web Developer. Highly organized, responsible, outgoing, technology savvy with ideas, adaptable in any scenario, and demonstrated capacity to improve oneself and others. <br />
+                        Since enrolling in the Programming Hero Web Development course, I've been creating code every day. Alhumdulillah! I can tell that I'm getting better every day. As if I were performing an assignment alone, I challenge myself with a new skill. Will contribute to open-source projects and will always teach what I know to others. It greatly aids my talent development. I set aside at least 6-8 hours every day for regular downtime, as per my timetable.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
 
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
 
     return (
-        <footer className='text-center mt-5'>
-            <p>Copyright © {year}</p>
+        <footer className='text-center mt-5 footer-all'>
+            <p><span className='footer-title'>পড়াই মামা</span> © {year}</p>
         </footer>
     );
 };
